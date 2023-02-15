9fbddb290cb70f52b7f18c75a62d3c7e067deb48
Cranky Bawler <6666lenia6666@mail.ru>
2023-02-15 13:58:59 +0300

    Задача №2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
-        public static void main(String[] args) {
-            task1();
-        }
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
@@ -10,13 +12,31 @@ public class Main {
         }
         return arr;
     }
-        public static void task1 () {
-            System.out.println("Задача 1");
-            int[] arr = generateRandomArray();
-            int sum = 0;
-            for (int element : arr) {
-                sum = sum + element;
-                            }
-            System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int element : arr) {
+            sum = sum + element;
         }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] arr = generateRandomArray();
+        int maxSum = -1;
+        int minSum = 200001;
+        for (final int current : arr) {
+            if (current > maxSum) {
+                maxSum = current;
+            }
+            if (current < minSum) {
+                minSum = current;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minSum + " рублей. Максимальная сумма трат за " +
+                "день составила " + maxSum + " рублей");
+    }
 }
